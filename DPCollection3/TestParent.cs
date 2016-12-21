188061601804a2af9d68eed7321d6f9e0aa2eb64
cool-Blue <cool.blue@y7mail.com>
2016-12-21 12:57:34 +1100

    Rebuilt after gitshit

--- DPCollection3/TestParent.cs
+++ DPCollection3/TestParent.cs
@@ -5,7 +5,7 @@ using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Runtime.CompilerServices;
-using CollectionBinding.Annotations;
+using JetBrains.Annotations;
 
 namespace CollectionBinding
 {
